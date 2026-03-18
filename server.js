5fa304e751f7ad0d953eed3cc5f304f9bc30927c
AnnaaGonzalez <annayeli@apphive.io>
2026-03-18 15:03:56 -0600

    server

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-const express = require("express");
-const multer = require("multer");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
 
 const app = express();
-const upload = multer();
-app.get("/mensaje", (req, res) => {
-  res.send("Esta ruta solo acepta POST con audio 🎤");
+app.use(cors());
+app.use(express.json());
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
-app.post("/mensaje", upload.single("audio"), (req, res) => {
-  console.log("Archivo recibido:");
-  console.log(req.file);
-
-  res.json({
-    ok: true,
-    mensaje: "Audio recibido correctamente",
+async function preguntarGemini(texto) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: texto,
   });
+
+  return response.text;
+}
+
+app.post("/mensaje", async (req, res) => {
+  try {
+    const { mensaje } = req.body;
+
+    const respuesta = await preguntarGemini(mensaje);
+
+    res.json({
+      botText: respuesta,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 });
 
 app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+  console.log("Servidor corriendo en http://localhost:3000");
 });
